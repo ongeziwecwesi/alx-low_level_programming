bc2a3fe8d291e8c908ea8f463a28e5ffc2220a5b
ongeziwe <ongeziwecwesi@gmail.com>
2023-09-08 03:25:35 -0700

    Add the 0-positive_or_negative.c script

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /**
- * Determines if a number is positive, negative or zero.
+ * main - Determines if a number is positive, negative or zero.
  *
  * Return: Always 0 (Success)
  */
