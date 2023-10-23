65441ef8005dfa6f2c7bfaed068dafb296f9329d
ongeziwecwesi <ongeziwecwesi@gmail.com>
2023-10-23 10:16:04 -0700

    Update

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,20 @@
+#ifndef LISTS_H
+#define LISTS_H
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * struct listint_s -singly linked list
+ * @n: integer data in the node
+ * @next: pointer to the next node
+ * description: singly linked list node structure 
+ * for integer data
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+
+#endif
