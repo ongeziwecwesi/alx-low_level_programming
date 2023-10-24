462c4efff35982c2bb95ae674cec4ce725e900bc
ongeziwecwesi <ongeziwecwesi@gmail.com>
2023-10-24 14:44:54 -0700

    Add the 9-insert_nodeint.c file

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,6 +1,4 @@
 #include "lists.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
  * insert_nodeint_at_index - inserts a new node at a given position in a list
@@ -12,28 +10,35 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new_node, *current = *head;
-	unsigned int count = 0;
+	unsigned int count;
+	listint_t new_node;
+	listint_t *current = *head;
+
+	new_node = malloc(sizeof(listint_t));
+	if (!new_node || !head)
+		return (NULL);
+
+	new_node->n;
+	new_node->next = NULL;
 
 	if (idx == 0)
-		return (add_ nodeint(head, n));
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
 
-	while (current != NULL)
+	for (count = 0; current && count < idx; count++)
 	{
 		if (count == idx - 1)
 		{
-			new_node = malloc(sizeof(listint_t));
-			if (new_node == NULL)
-				return (NULL);
-
-			new_node->n = n;
 			new_node->next = current->next;
 			current->next = new_node;
 			return (new_node);
-
 		}
-		current = current->next;
-		count++;
+		else
+			current = current->next;
 	}
+
 	return (NULL);
 }
