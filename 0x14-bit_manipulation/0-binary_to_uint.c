1c2b3216d52a5bc97914ab5dccee7856b3a53a74
ongeziwecwesi <ongeziwecwesi@gmail.com>
2023-10-26 09:31:50 -0700

    Add the 0-binary_to_uint.c file

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -14,10 +14,13 @@ unsigned int binary_to_uint(const char *b)
 	if (!b)
 		return (0);
 
-	for (c = 0; b[c]; c++)
+	for (c = 0; b[c] != '0'; c++)
 	{
 		if (b[c] != '0' && b[c] != '1')
 			return (0);
+	}
+	for (c = 0; b[c] != '0'; c++)
+	{
 		result <<= 1;
 		if (b[c] == '1')
 			result += 1;
