938649513245848921afa3dea42796122af5a9d9
ongeziwecwesi <ongeziwecwesi@gmail.com>
2023-10-30 19:56:10 -0700

    Add the 0-read_textfile.c file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,48 +1,37 @@
 #include "main.h"
 
 /**
- * read_textfile - Read and print text from a file
- * @filename: Name of the file to read
- * @letters: Number of letters to read and print
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename.
+ * @letters: numbers of letters printed.
  *
- * Return: Number of letters read and printed, 0 on failure
+ * Return: numbers of letters printed. It fails, returns 0.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t bytes_read, bytes_written;
+	ssize_t nrd, nwr;
 	char *buffer;
 
-	if (filename == NULL)
+	if (!filename)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
+
 	if (fd == -1)
 		return (0);
 
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
-	{
-		close(fd);
+	buffer = malloc(sizeof(char) * (letters));
+	if (!buffer)
 		return (0);
-	}
 
-	bytes_read = read(fd, buffer, letters);
-	if (bytes_read == -1)
-	{
-		free(buffer);
-		close(fd);
-		return (0);
-	}
-	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-	if (bytes_written == -1 || (size_t)bytes_written != (size_t)bytes_read)
-	{
-		free(buffer);
-		close(fd);
-		return (0);
-	}
+	nrd = read(fd, buffer, letters);
+	nwr = write(STDOUT_FILENO, buffer, nrd);
+
+	close(fd);
 
 	free(buffer);
-	close(fd);
-	return (bytes_read);
+
+	return (nwr);
 }
+
