0d93f2fdaac2540c09b7c6e48dce80739b8c11af
ongeziwecwesi <ongeziwecwesi@gmail.com>
2023-10-31 08:57:12 -0700

    Add the 2-append_text_to_file.c file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,7 +10,8 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, bytes_written = 0;
+	int fd;
+	ssize_t bytes_written = 0;
 
 	if (filename == NULL)
 		return (-1);
