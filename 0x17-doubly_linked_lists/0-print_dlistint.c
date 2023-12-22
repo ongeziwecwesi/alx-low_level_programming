20d260fdf450715f9492bbf1c3f8f7780a73f46f
ongeziwe <ongeziwecwesi@gmail.com>
2023-12-22 07:18:40 -0800

    Add the 0-print_dlistint.c file

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,25 +9,14 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-    /*Variable to store the number of nodes in the list.*/
-    size_t nodes = 0;
+	size_t nodes = 0;
 
+	while (h)
+	{
+		nodes++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
 
-    while (h)
-    {
-        /*Increment the node count.*/
-        nodes++;
-
-
-        /*Print the value of the current node.*/
-        printf("%d\n", h->n);
-
-
-        /*Move to the next node.*/
-        h = h->next;
-    }
-
-
-    /*Return the total number of nodes in the list.*/
-    return (nodes);
+	return (nodes);
 }
