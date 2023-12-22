bbccd8873adb697be59a585ba7701250786fa449
ongeziwe <ongeziwecwesi@gmail.com>
2023-12-22 07:13:52 -0800

    Add the 7-insert_dnodeint.c file

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,35 +12,30 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-    dlistint_t *tmp = *h, *new;
+	dlistint_t *tmp = *h, *new;
 
-    /* If the index is 0, insert at the beginning of the list. */
-    if (idx == 0)
-        return (add_dnodeint(h, n));
+	if (idx == 0)
+		return (add_dnodeint(h, n));
 
-    /* Traverse the list to reach the desired position. */
-    for (; idx != 1; idx--)
-    {
-        tmp = tmp->next;
-        if (tmp == NULL)
-            return (NULL);
-    }
+	for (; idx != 1; idx--)
+	{
+		tmp = tmp->next;
+		if (tmp == NULL)
+			return (NULL);
+	}
 
-    /* If the desired position is at the end, insert at the end of the list. */
-    if (tmp->next == NULL)
-        return (add_dnodeint_end(h, n));
+	if (tmp->next == NULL)
+		return (add_dnodeint_end(h, n));
 
-    /* Allocate memory for the new node. */
-    new = malloc(sizeof(dlistint_t));
-    if (new == NULL)
-        return (NULL);
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
 
-    /* Set the value of the new node and update the links. */
-    new->n = n;
-    new->prev = tmp;
-    new->next = tmp->next;
-    tmp->next->prev = new;
-    tmp->next = new;
+	new->n = n;
+	new->prev = tmp;
+	new->next = tmp->next;
+	tmp->next->prev = new;
+	tmp->next = new;
 
-    return (new);
+	return (new);
 }
